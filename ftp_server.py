dfd1b385b434a2793ba58b2f0a5ff341185ba8e1
BrotherB1 <bassettl@mail.gvsu.edu>
2019-02-11 18:29:24 -0500

    Minor edits

--- ftp_server.py
+++ ftp_server.py
@@ -1,10 +1,6 @@
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
-import socket
-import ipaddress
-
-ipaddress.ip_address('192.168.0.1')
 
 authorizer = DummyAuthorizer()
 authorizer.add_user("user", "12345", "/Users/lbassett", perm="elradfmw")
