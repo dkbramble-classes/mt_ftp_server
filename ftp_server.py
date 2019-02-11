4e108f54e752ffbeeccc748f795794d374798319
BrotherB1 <bassettl@mail.gvsu.edu>
2019-02-11 18:25:57 -0500

    Takes user input for server host

--- ftp_server.py
+++ ftp_server.py
@@ -1,6 +1,10 @@
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
+import socket
+import ipaddress
+
+ipaddress.ip_address('192.168.0.1')
 
 authorizer = DummyAuthorizer()
 authorizer.add_user("user", "12345", "/Users/lbassett", perm="elradfmw")
@@ -10,5 +14,8 @@ handler = FTPHandler
 handler.authorizer = authorizer
 handler.banner = "You have connected sucessfully!"
 
-server = FTPServer(("35.40.65.30", 1026), handler) #Make IP the IP of the host machine
+IP = input("Please Enter the IP address you want as server: ")
+print(IP)
+server = FTPServer((IP, 1026), handler) #Make IP the IP of the host machine
+
 server.serve_forever()
